373bf3bd089602f0876a9f0666499842d7a27b30
piskas <luis.arellanovz@gmail.com>
2018-04-27 16:32:35 -0300

    Ejercicios 2018-04-21

---
+++ App_20180421_Conexion/App_20180421_Conexion/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace App_20180421_Conexion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strConnection = "Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=20180421_PrograAV;Data Source=.";
+            string strQuery = "";
+
+            OleDbConnection connection = new OleDbConnection(strConnection);
+            OleDbCommand command = new OleDbCommand("", connection);
+            OleDbDataReader reader;
+
+            try
+            {
+                strQuery = "SELECT * FROM Alumnos";
+                command.CommandText = strQuery;
+                connection.Open();
+
+                if (connection.State == ConnectionState.Open)
+                {
+                    int contadorDatos = 0;
+                    reader = command.ExecuteReader();
+
+                    Console.WriteLine("========================================== Lista de Alumnos ==========================================");
+                    Console.WriteLine(" \t    Rut\t\t\t Nombre\t\t\tApellido\t\tPromedio");
+                    Console.WriteLine("======================================================================================================");
+                    //Console.WriteLine(" \t===\t\t\t======\t\t\t========\t\t========");
+
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(" \t{0}\t\t{1}\t\t{2}\t\t\t{3}",
+                            reader.GetString(0).Trim(),
+                            reader.GetString(1).Trim(),
+                            reader.GetString(2).Trim(),
+                            reader.GetValue(3).ToString().Trim());
+                        contadorDatos++;
+                    }
+                    if (contadorDatos == 0)
+                        Console.WriteLine("No hay datos en la tabla alumno");
+                }
+                else
+                {
+                    Console.WriteLine("La conexion no se ha podido establecer");
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Se ha producido la excepcion: {0}", ex.Message);
+            }
+
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                    connection.Close();
+            }
+
+            Console.ReadKey();
+        }
+    } 
+}
---
+++ App_20180421_InsertarDB/App_20180421_InsertarDB/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+
+namespace App_20180421_InsertarDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strConnection = "Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=PrograAV_20180421;Data Source=.";
+            string strQuery = "";
+            OleDbConnection connection = new OleDbConnection(strConnection);
+            OleDbCommand command = new OleDbCommand("", connection);
+            
+            string rut = "", nombre = "", apellido = "", respuesta = "";
+            float promedio;
+            bool continua = true;
+
+            try
+            {
+                while (continua)
+                {
+                    Console.Clear();
+                    Console.WriteLine("============== Ingreso de Datos de Alumnos ==============\n");
+
+                    Console.Write("Ingrese Rut:");
+                    rut = Console.ReadLine();
+
+                    Console.Write("Ingrese Nombre:");
+                    nombre = Console.ReadLine();
+
+                    Console.Write("Ingrese Apellido:");
+                    apellido = Console.ReadLine();
+
+                    Console.Write("Ingrese Promedio:");
+                    promedio = float.Parse(Console.ReadLine());
+
+
+                    // Se escribe la consulta
+                    strQuery = String.Format("INSERT INTO ALUMNOS VALUES('{0}','{1}','{2}',{3})",
+                        rut,
+                        nombre,
+                        apellido,
+                        promedio);
+
+                    command.CommandText = strQuery;
+                    if (connection.State != ConnectionState.Open)
+                        connection.Open();
+
+
+                    // Se verifica estado de conexion
+                    if (connection.State == ConnectionState.Open)
+                    {
+                        int contadorDatos = command.ExecuteNonQuery();
+
+                        if (contadorDatos == 0)
+                            Console.WriteLine("Los datos no fueron insertador en la tabla alumno");
+                        else
+                            Console.WriteLine("Los datos se insertaron correcatmente");
+                    }
+                    else
+                    {
+                        Console.WriteLine("La conexion no se ha podido establecer");
+                    }
+
+
+                    do
+                    {
+                        Console.Clear();
+                        Console.Write("\n\nDesea ingresar otro alumno [S/N]:");
+                        respuesta = Console.ReadLine().Trim().ToUpper();
+
+                        if (respuesta.Equals("S"))
+                            continua = true;
+                        else if (respuesta.Equals("N"))
+                            continua = false;
+                        else
+                        {
+                            Console.WriteLine("Respuesta erronea debe ser S o N");
+                            Console.ReadKey();
+                        }
+                    } while (!respuesta.Equals("S") && !respuesta.Equals("N"));
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Se ha producido la excepcion: {0}", ex.Message);
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                    connection.Close();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
