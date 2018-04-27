818a3fe3cb50894f5aeca3086b7888ae82ffdc53
piskas <luis.arellanovz@gmail.com>
2018-04-27 16:31:56 -0300

    Ejercicios 2018-04-07

---
+++ App_20180407/App_20180407/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace App
+{
+    class Program
+    {
+        // Ejercicio 1
+        public static void Main()
+        {
+            string nombre = "";
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.WriteLine("Ingrese su nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Hola {0}", nombre);
+            Console.WriteLine("Presione una tecla");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        //// Ejercicio 2
+        //public static void Main(string[] args)
+        //{
+        //    if (args.Length == 1)
+        //    {
+        //        Console.WriteLine("Hola " + args[0] + "!!");
+        //    }
+        //    else if (args.Length > 1)
+        //    {
+        //        for (int i = 0; i < args.Length; i++)
+        //            Console.WriteLine("Hola " + args[i]);
+        //    } 
+        //    else 
+        //    {
+        //        Console.WriteLine("Hola raton con cola");
+        //    }
+        //    Console.ReadKey();
+        //}
+
+        // Ejercicio 3
+
+    }
+}
---
+++ App_20180407_Arreglos/App_20180407_Arreglos/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace App_20180407_Arreglos
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int[,,] buses = new int[3, 5, 40];
+            string respuesta = "S";
+            int i, j, k, valor = 0, total = 0;
+
+            try {
+                while (respuesta == "S")
+                {
+                    try {
+                        Console.Write("Ingrese n°Sucursal:");
+                        i = Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Ingrese n°Bus:");
+                        j = Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Ingrese n°Asiento:");
+                        k = Convert.ToInt32(Console.ReadLine());
+                        
+                        if (i > 3 || i <= 0)
+                            throw new Exception("N° Sucursal fuera de rango\n");
+                        if (j > 5 || j <= 0)
+                            throw new Exception("N° Bus fuera de rango\n");
+                        if (k > 40 || k <= 0)
+                            throw new Exception("N° Asiento fuera de rango\n");
+                        
+                        Console.Write("Ingrese 1, No Disponible:");
+                        valor = Convert.ToInt32(Console.ReadLine());
+                        
+                        if (valor!=1)
+                            throw new Exception("N° no Valido");
+
+                        buses[i - 1, j - 1, k - 1] = valor;
+                        Console.Write("Desea Continuar S/N");
+                        respuesta = Console.ReadLine().Trim().ToUpper();
+
+                    }
+                    catch(Exception e)
+                    {
+                        Console.Write(e.Message);
+                    }
+                }
+                for (i = 0; i < buses.GetLength(0); i++)
+                {
+                    for(j = 0; j < buses.GetLength(0); j++)
+                    {
+                        for (k = 0; k < buses.GetLength(0); k++)
+                            if (buses[i, j, k] == 0)
+                                total += 1;
+                    }
+                    Console.Write("Total de asientos disponibles sucursal {0} es {1}", i + 1, total);
+                    total = 0;
+                }
+            }
+            catch(Exception e)
+            {
+                Console.Write(e.Message);
+            }
+        }
+    }
+}
